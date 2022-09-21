691e44aff9a2a7ea119098037c2bb404c0afd306
trankhoadev <trankhoadev@gmail.com>
2022-09-21 10:24:50 +0700

    Accomplished setup project and perform create table on mysql

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const userName = 'root';
+const passWord = '';
+const database = 'db-crud-with-sequelize';
+const host = 'localhost'
+const node_env = 'dev'
+
+const config = {
+    dev: {
+        db: {
+            userName,
+            passWord,
+            database,
+            host
+        }
+    },
+    test: {},
+    prod: {}
+};
+
+module.exports = config[node_env]
